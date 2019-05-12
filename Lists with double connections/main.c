87fcd246ee629a7ee0ca95bbb8aabc7265119740
Anthony Borushko <twinpixxx2005@gmail.com>
2019-05-12 21:51:19 +0300

    feat: implement new structure

--- Lists with double connections/main.c
+++ Lists with double connections/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+struct cars {
+	char* carModelName;
+	int* carPower;
+	struct cars* nextCar;
+	struct cars* previousCar;
+}; //cars structure
+
+
 int main() {
 	printf("Hello, World!\n");
 	return 0;
